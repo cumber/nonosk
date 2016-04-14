28d9f9c0372e00ca3e4f7889267473ae27c38115
Benjamin Mellor <ben@smokingkangaroo.com>
2016-04-14 14:07:08 +1000

    use singletons to actually initialise a blank puzzle

--- src/Skapix/Puzzle.hs
+++ src/Skapix/Puzzle.hs
@@ -11,12 +11,14 @@
 module Skapix.Puzzle
 where
 
-import Data.Vector.Sized (Vector(Nil, (:-)))
+import Data.Type.Natural (Nat, SNat, intToNat)
+
+import Data.Vector.Sized (Vector)
 import qualified Data.Vector.Sized as V
 
-import Data.Type.Natural
+import Numeric.Natural (Natural)
 
-import Numeric.Natural
+import Data.Singletons (SomeSing(SomeSing), toSing)
 
 
 newtype Grid (r :: Nat) (c :: Nat) (a :: *)
@@ -34,6 +36,10 @@ toLists :: Grid r c a -> [[a]]
 toLists = V.toList . V.map V.toList . unGrid
 
 
+constGrid :: SNat r -> SNat c -> a -> Grid r c a
+constGrid r c = Grid . V.replicate r . V.replicate c
+
+
 instance Eq a => Eq (Puzzle a)
   where Puzzle g == Puzzle h = toLists g == toLists h
 
@@ -42,31 +48,9 @@ instance Functor Puzzle
   where fmap f (Puzzle (Grid v)) = Puzzle . Grid . V.map (V.map f) $ v
 
 
-data Some :: (k -> *) -> *
-  where Some :: f x -> Some f
-
-
-smap :: (forall a. f a -> g (q a)) -> Some f -> Some g
-smap f (Some x) = Some (f x)
-
-sbind :: (forall a. f a -> Some g) -> Some f -> Some g
-sbind f (Some x) = f x
-
-elim :: (forall a. f a -> c) -> Some f -> c
-elim f (Some x) = f x
-
-what :: (forall r a. f a -> (forall b. g b -> r g) -> r g) -> Some f -> Some g
-what f (Some x) = f x Some
-
-cps :: (f a -> g b) -> f a -> (forall c. g c -> r) -> r
-cps f a c = c (f a)
-
-
-
-fromListSome :: [a] -> Some (Vector a)
-fromListSome = foldr (\x -> smap (x :-)) (Some Nil)
-
-
 initPuzzle :: a -> [Natural] -> [Natural] -> Puzzle a
 initPuzzle cell rowHints colHints
-  = Puzzle (Grid Nil)
+  = let nRows = intToNat $ length rowHints
+        nCols = intToNat $ length colHints
+    in  case (toSing nRows, toSing nCols) of
+          (SomeSing r, SomeSing c) -> Puzzle $ constGrid r c cell
