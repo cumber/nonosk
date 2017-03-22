5c6c1e3b7d62a268bd35ce5201c524dddc5f517f
Benjamin Mellor <ben@smokingkangaroo.com>
2017-03-22 17:32:15 +1000

    more vector tests

--- tests/Scaffolding/SmallCheck.hs
+++ tests/Scaffolding/SmallCheck.hs
@@ -4,12 +4,11 @@
 
 module Scaffolding.SmallCheck
   ( Element
+  , Element2
   , vector
-  , list
+  , vector2
   )
 where
-
-import Data.List ( inits )
 
 import GHC.Natural ( Natural )
 
@@ -24,12 +23,14 @@ import Data.Indexed.Index ( Index
                           )
 
 import Data.Indexed.Some ( Some (Some)
+                         , Some2 (Some2)
                          , forSome
                          )
 
 import Data.Indexed.Vector ( Vector
                            , fromIndices
                            )
+import Data.Indexed.Vector2 ( Vector2 (Vector2) )
 
 
 instance Monad m => Serial m (Some Index ())
@@ -45,8 +46,19 @@ instance Show Element
   where show (Element tag index) = tag ++ show index
 
 
+data Element2 = Element2 String Natural Natural
+  deriving (Eq)
+
+instance Show Element2
+  where show (Element2 tag x y) = tag ++ "_" ++ show x ++ "_" ++ show y
+
+
 vector :: Monad m => String -> Series m (Some Vector Element)
 vector tag = forSome (\i -> Some $ fromIndices i (Element tag)) <$> series
 
-list :: Monad m => String -> Series m [Element]
-list tag = generate $ \d -> inits $ Element tag <$> [0 .. fromIntegral d]
+vector2 :: Monad m => String -> Series m (Some2 Vector2 Element2)
+vector2 tag
+  = let makeRow len r = fromIndices len (Element2 tag r)
+        makeVectors (Some rows, Some cols)
+          = Some2 . Vector2 $ fromIndices rows (makeRow cols)
+     in makeVectors <$> series
--- tests/Test/Data/Indexed/Vector.hs
+++ tests/Test/Data/Indexed/Vector.hs
@@ -1,10 +1,17 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DataKinds
+           , RankNTypes
+           , ScopedTypeVariables
+           , TypeApplications
+           , TypeFamilies
+           , TypeOperators
+  #-}
 
 module Test.Data.Indexed.Vector
   ( tests )
 where
 
-import Control.Monad ( (<=<) )
+import Control.Arrow ( second )
+
 import Data.Foldable ( toList )
 
 import qualified Data.List as List
@@ -18,18 +25,27 @@ import qualified Test.Tasty.SmallCheck as SC
 
 import Data.Indexed.Index ( Index
                           , index
+                          , index'
+                          , switchZero'
                           )
 
-import Data.Indexed.Some ( Some
-                         , forSome
-                         , liftPlus
+import Data.Indexed.Nat ( KnownNat
+                        , type (>=)
+                        )
+
+import Data.Indexed.Some ( Some (Some)
+                         , Some2 (Some2)
                          )
 
-import Data.Indexed.Vector ( Vector )
+import Data.Indexed.Vector ( Vector (Nil) )
 import qualified Data.Indexed.Vector as Vector
 
+import Data.Indexed.Vector2 ( Vector2 (Vector2) )
+
 import Scaffolding.SmallCheck ( Element
+                              , Element2
                               , vector
+                              , vector2
                               )
 
 
@@ -41,24 +57,87 @@ tests = testGroup "Vector" [ scProps
 scProps
   = testGroup "SmallCheck"
       [ appendList
+      , appendOperatorList
+
+      , testAgainstNonEmptyList "Vector.head  vs  List.head"
+          (Vector.head) (List.head)
+      , testAgainstNonEmptyList "Vector.last  vs  List.last"
+          (Vector.last) (List.last)
+      , testAgainstNonEmptyList "Vector.tail  vs  List.tail"
+          (toList . Vector.tail) (List.tail)
+      , testAgainstNonEmptyList "Vector.init  vs  List.init"
+          (toList . Vector.init) (List.init)
+      , testAgainstNonEmptyList "Vector.init vs List.init"
+          (Just . second toList . Vector.uncons) (List.uncons)
+
+      , transposeList
+
       , replicateList
       ]
+
+
+testAgainstList
+  :: Eq r
+  =>    String
+     -> (forall n. KnownNat n => Vector n Element -> r)
+     -> ([Element] -> r)
+     -> TestTree
+testAgainstList label vectorOp listOp
+  = SC.testProperty label $ SC.over (vector "X") test
+  where test (Some xs) = vectorOp xs == (listOp . toList) xs
+
+
+testAgainstNonEmptyList
+  :: Eq r
+  =>    String
+     -> (forall n. (KnownNat n, n >= 1) => Vector n Element -> r)
+     -> ([Element] -> r)
+     -> TestTree
+testAgainstNonEmptyList label vectorOp listOp
+  = SC.testProperty label $ SC.over (vector "X") test
+  where test :: Some Vector Element -> Bool
+        test (Some (xs :: Vector n Element))
+          = switchZero' @ n
+              (null . toList $ xs)
+              (vectorOp xs == (listOp . toList) xs)
 
 
 over2 s1 s2 q = SC.over s1 (\x -> SC.over s2 (\y -> q x y))
 
 
 appendList
-  = SC.testProperty "toList (Vector.append xs ys) == toList xs ++ toList ys"
+  = SC.testProperty "Vector.append xs ys  vs  toList xs ++ toList ys"
       $ over2 (vector "X") (vector "Y") test
   where test :: Some Vector Element -> Some Vector Element -> Bool
-        test xs ys
-          = toList (liftPlus Vector.append xs ys) == toList xs ++ toList ys ++ toList ys
+        test (Some xs) (Some ys)
+          = toList (Vector.append xs ys) == toList xs ++ toList ys
+
+appendOperatorList
+  = SC.testProperty "xs Vector.++ ys  vs  toList xs ++ toList ys"
+      $ over2 (vector "X") (vector "Y") test
+  where test :: Some Vector Element -> Some Vector Element -> Bool
+        test (Some xs) (Some ys)
+          = toList (xs Vector.++ ys) == toList xs ++ toList ys
+
+
+transposeList
+  = SC.testProperty "Vector.transpose  vs  List.transpose"
+      $ SC.over (vector2 "X") test
+  where test :: Some2 Vector2 Element2 -> Bool
+        test (Some2 (Vector2 xss :: Vector2 r c Element2))
+          = let v = (fmap toList . toList . Vector.transpose) xss
+                l = (List.transpose . fmap toList . toList) xss
+                -- Vector.transpose faithfully transforms 0 rows of n columns
+                -- into n empty rows, whereas List.transpose has no way to
+                -- tell any of the zero row values of type [[a]] apart, so
+                -- always produces []
+                emptyRows= List.genericReplicate (index' @ c) []
+             in v == l || (index' @ r == 0 && index' @ c >= 1 && v == emptyRows)
+
 
 replicateList
-  = SC.testProperty "toList . Vector.replicate == List.replicate" test
+  = SC.testProperty "Vector.replicate  vs  List.replicate" test
   where test :: Bool -> Some Index () -> Bool
-        test x
-          = forSome (\n -> toList (Vector.replicate n x)
-                            == List.replicate (fromIntegral $ index n) x
-                    )
+        test x (Some n)
+          = toList (Vector.replicate n x)
+              == List.replicate (fromIntegral $ index n) x
