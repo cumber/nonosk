27c93085e5d83d429589b3cd49f385024f32fe38
Benjamin Mellor <ben@smokingkangaroo.com>
2017-04-24 10:46:44 +1000

    add an alternative operator for type level multiplication

--- src/Data/Indexed/Nat.hs
+++ src/Data/Indexed/Nat.hs
@@ -6,7 +6,7 @@
 
 module Data.Indexed.Nat
   ( Nat, KnownNat
-  , type (+), type (-), type (*)
+  , type (+), type (-), type (*), type (×)
   , type (<=), type (>=)
   , type (<), type (>)
   )
@@ -20,3 +20,8 @@ import GHC.TypeLits ( Nat, KnownNat
 type n > m = (n <=? m) ~ False
 type n < m = m > n
 type n >= m = m <= n
+
+-- The multiplication type operator confuses a lot of tooling (haskell-src-exts,
+-- hlint, etc) due to special parse rules needed for the kind * (Type).
+-- Export a unicode equivalnet for convenience.
+type a × b = a GHC.TypeLits.* b
